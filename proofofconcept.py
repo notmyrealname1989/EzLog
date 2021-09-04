801f86a11c5b9fb4fc44d3d3f2b42c61990ebcb1
Andrew Ayash <andrew.ayash@gmail.com>
2021-09-04 11:00:15 -0400

    sys.arg revision

--- proofofconcept.py
+++ proofofconcept.py
@@ -4,16 +4,22 @@ import sys
 import os
 import subprocess
 
-inputdir = "/home/kali/ezlog"
-with open("outfile.txt","w") as outfile:
-    for pcapFilename in os.listdir(inputdir):
-         if not pcapFilename.endswith(".pcap"):
-            continue
-         subprocess.run(["tshark", "-r",
-             os.path.join(inputdir, pcapFilename),
-             "-q", "-z", "io,stat,1", "--export-objects", "http,destdir2"],
-            stdout=outfile, check=True)   
+inputdir = sys.argv[1]
 
+def ezlog(inputdir):
+    with open("outfile.txt","w") as outfile:
+        for pcapFilename in os.listdir(inputdir):
+             if not pcapFilename.endswith(".pcap"):
+                continue
+             subprocess.run(["tshark", "-r",
+                os.path.join(inputdir, pcapFilename),
+                "-q", "-z", "io,stat,1", "--export-objects", "http,destdir2"],
+                stdout=outfile, check=True)
+
+def main():
+    ezlog(inputdir)
+
+main()
 
 
 #hello
