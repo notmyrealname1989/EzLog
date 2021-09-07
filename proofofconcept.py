4cf8f114d2d0db4a2f156654c71c857741dbedff
Andrew Ayash <andrew.ayash@gmail.com>
2021-09-07 18:29:40 -0400

    newest version

--- proofofconcept.py
+++ proofofconcept.py
@@ -5,6 +5,9 @@ import os
 import subprocess
 
 inputdir = sys.argv[1]
+outputdir = f"{inputdir}_exported_objects"
+
+
 
 def ezlog(inputdir):
     with open("outfile.txt","w") as outfile:
@@ -13,8 +16,16 @@ def ezlog(inputdir):
                 continue
              subprocess.run(["tshark", "-r",
                 os.path.join(inputdir, pcapFilename),
-                "-q", "-z", "io,stat,1", "-z", "credentials", "--export-objects", "http,destdir2"],
+                "-q", "-z", "io,stat,1", "-z", "credentials", "--export-objects", "http,/home/kali/ezlog/destdir2"],
                 stdout=outfile, check=True)
+            
+    os.rename("/home/kali/ezlog/destdir2", outputdir)
+    for filename in os.listdir(outputdir):
+        picture = filename.endswith(".jpg")
+        if filename is picture:
+            os.mkdir(f"/home/kali/ezlog/{outputdir}/jpg_files", 0o666)
+            move(filename, f"/home/kali/ezlong/{outputdir}/{jpg_files}")
+
 
 def main():
     ezlog(inputdir)
