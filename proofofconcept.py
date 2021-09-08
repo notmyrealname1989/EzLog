f40c148e4138884b9cdba5a3966712b133055e1d
Andrew Ayash <andrew.ayash@gmail.com>
2021-09-07 21:37:10 -0400

    mvp

--- proofofconcept.py
+++ proofofconcept.py
@@ -2,7 +2,8 @@
 
 import sys
 import os
-import subprocess
+import subprocess as s
+import shutil 
 
 inputdir = sys.argv[1]
 outputdir = f"{inputdir}_exported_objects"
@@ -14,17 +15,29 @@ def ezlog(inputdir):
         for pcapFilename in os.listdir(inputdir):
              if not pcapFilename.endswith(".pcap"):
                 continue
-             subprocess.run(["tshark", "-r",
+             s.run(["tshark", "-r",
                 os.path.join(inputdir, pcapFilename),
                 "-q", "-z", "io,stat,1", "-z", "credentials", "--export-objects", "http,/home/kali/ezlog/destdir2"],
                 stdout=outfile, check=True)
             
     os.rename("/home/kali/ezlog/destdir2", outputdir)
     for filename in os.listdir(outputdir):
-        picture = filename.endswith(".jpg")
-        if filename is picture:
-            os.mkdir(f"/home/kali/ezlog/{outputdir}/jpg_files", 0o666)
-            move(filename, f"/home/kali/ezlong/{outputdir}/{jpg_files}")
+        process_list = ['jpg', 'png', 'gif', 'exe']
+        for item in process_list:
+            if filename.endswith(item):
+                if not os.path.exists(f"/home/kali/ezlog/{outputdir}/{inputdir}_{item}"):
+                    os.makedirs(f"/home/kali/ezlog/{outputdir}/{inputdir}_{item}")
+                if os.path.exists(f"/home/kali/ezlog/{outputdir}/{inputdir}_{item}"):
+               # else:
+                    shutil.move(f"/home/kali/ezlog/{outputdir}/{filename}", f"/home/kali/ezlog/{outputdir}/{inputdir}_{item}")
+
+
+
+               # variable_1 = f"{filename}"
+               # s.run(["mkdir", f"/home/kali/ezlog/{outputdir}/{inputdir}_{item}"])
+               # s.run(["mv", f"{variable_1}", f"/home/kali/ezlog/{outputdir}/{inputdir}_{item}"])
+            
+            
 
 
 def main():
@@ -34,3 +47,4 @@ main()
 
 
 #hello
+
