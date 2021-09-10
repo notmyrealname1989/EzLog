0f7f4e90531469accfc7453c6dac63537112f756
Andrew Ayash <andrew.ayash@gmail.com>
2021-09-09 21:37:28 -0400

    move other works

--- proofofconcept.py
+++ proofofconcept.py
@@ -9,31 +9,42 @@ import shutil
 
 
 def ezlog(inputdir):
+    other_path = f"/{inputdir}" + "_other"
     outputdir = f"{inputdir}_exported_objects"
+    http_output = "http," + outputdir + other_path
+    final_path = outputdir + other_path
+   # print(http_output)
+   # if not os.path.exists("/home/kali/ezlog"):
+       # os.makedirs("/home/kali/ezlog")
     with open("outfile.txt","w") as outfile:
+      
        # for pcapFilename in os.listdir(inputdir):
            # if not pcapFilename.endswith(".pcap"):
 
         s.run(["tshark", "-r", inputdir,
-             "-q", "-z", "io,stat,1", "-z", "credentials", "--export-objects", "http,/home/kali/ezlog/destdir2"],
+             "-q", "-z", "io,stat,1", "-z", "credentials", "--export-objects", http_output],
                 stdout=outfile, check=True)
             
-    os.rename("/home/kali/ezlog/destdir2", outputdir)
+   # os.rename("/home/kali/ezlog/destdir2", outputdir)
     
-    for filename in os.listdir(outputdir):
+    for filename in os.listdir(final_path):
         process_list = ['jpg', 'png', 'gif', 'exe', 'js', 'swf', 'css', 'xml', 'htm', 'html', 'php', 'txt', 'com', 'jsp', 'jpeg']
+       # other_files = f"/home/kali/ezlog/{outputdir}/{filename}"
+       # other_path = f"/home/kali/ezlong/{outputdir}/{inputdir}_other"
         for item in process_list:
             #filename = filename.lower()
             if filename.endswith(item) or filename.endswith(item.upper()):
                 if not os.path.exists(f"/home/kali/ezlog/{outputdir}/{inputdir}_{item}"):
                     os.makedirs(f"/home/kali/ezlog/{outputdir}/{inputdir}_{item}")
                 if os.path.exists(f"/home/kali/ezlog/{outputdir}/{inputdir}_{item}"):
-                    shutil.move(f"/home/kali/ezlog/{outputdir}/{filename}", f"/home/kali/ezlog/{outputdir}/{inputdir}_{item}")
-                else:
-                    if not os.path.exists(f"/home/kali/ezlog/{outputdir}/{inputdir}_other"):
-                        os.makedirs(f"/home/kali/ezlog/{outputdir}/{inputdir}_other")
-                    if os.path.exists(f"/home/kali/ezlog/{outputdir}/{inputdir}_other"):
-                        shutil.move(f"/home/kali/ezlog/{outputdir}/{filename}", f"/home/kali/ezlog/{outputdir}/{inputdir}_other")
+                    shutil.move(f"/home/kali/ezlog/{outputdir}/{other_path}/{filename}", f"/home/kali/ezlog/{outputdir}/{inputdir}_{item}")
+           # else:
+               # if not os.path.exists(other_path):
+                #    os.makedirs(other_path)
+                #if os.path.exists(other_path):
+                    #shutil.move(other_files, other_path)
+                #    s.run(["mkdir", other_path])
+                 #   s.run(["mv", other_files, other_path])
 
 
 
