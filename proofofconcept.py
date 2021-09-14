acf2636a4deec5e1439656cdcdcb16e763312624
Andrew Ayash <andrew.ayash@gmail.com>
2021-09-14 14:37:02 -0400

    new lines out, blank files out

--- proofofconcept.py
+++ proofofconcept.py
@@ -13,25 +13,50 @@ def ezlog(inputdir):
     outputdir = f"{inputdir}_ez"
     http_output = "http," + outputdir + other_path
     final_path = outputdir + other_path
-    pcap_stats = f"{inputdir}_stats.txt"
+    pcap_credentials = f"{inputdir}_credentials.txt"
+    pcap_smtp = f"{inputdir}_smtp.txt"
    # print(http_output)
    # if not os.path.exists("/home/kali/ezlog"):
        # os.makedirs("/home/kali/ezlog")
-    with open(pcap_stats,"w") as outfile:
+    with open(pcap_credentials,"w") as outfile:
       
        # for pcapFilename in os.listdir(inputdir):
            # if not pcapFilename.endswith(".pcap"):
 
         s.run(["tshark", "-r", inputdir,
-             "-q", "-z", "io,stat,1", "-z", "credentials", "--export-objects", http_output],
+             "-q", "-T", "fields", "-e", "http.authbasic", "--export-objects", http_output],
                 stdout=outfile, check=True)
-        shutil.move(pcap_stats, outputdir)
+       
 
-            
+    stuff = []
+    with open(pcap_credentials, 'r') as open_file:
+        for line in open_file:
+            stuff.append(line)
+    
+    newstuff = [item for item in stuff if item != '\n']
+
+    with open(pcap_credentials, 'w') as open_file:
+        for item in newstuff:
+               open_file.write(item)
+        shutil.move(pcap_credentials, outputdir)
+        
+    with open(f"/home/kali/ezlog/{outputdir}/{pcap_credentials}", 'r') as open_file:
+        if os.stat(f"/home/kali/ezlog/{outputdir}/{pcap_credentials}").st_size == 0:
+            os.remove(f"/home/kali/ezlog/{outputdir}/{pcap_credentials}")
+           
+        
+    with open(pcap_smtp,"w") as outfile:
+        s.run(["tshark", "-Y", "smtp", "-r", inputdir], stdout=outfile, check=True)
+        shutil.move(pcap_smtp, outputdir)
+
+    with open(f"/home/kali/ezlog/{outputdir}/{pcap_smtp}", 'r') as open_file:
+        if os.stat(f"/home/kali/ezlog/{outputdir}/{pcap_smtp}").st_size == 0:
+            os.remove(f"/home/kali/ezlog/{outputdir}/{pcap_smtp}")
+
    # os.rename("/home/kali/ezlog/destdir2", outputdir)
     
     for filename in os.listdir(final_path):
-        process_list = ['jpg', 'png', 'gif', 'exe', 'js', 'swf', 'css', 'xml', 'htm', 'html', 'php', 'txt', 'com', 'jsp', 'jpeg']
+        process_list = ['jpg', 'png', 'gif', 'exe', 'js', 'swf', 'css', 'xml', 'htm', 'html', 'php', 'txt', 'com', 'jsp', 'jpeg', 'mp3']
        # other_files = f"/home/kali/ezlog/{outputdir}/{filename}"
        # other_path = f"/home/kali/ezlong/{outputdir}/{inputdir}_other"
         for item in process_list:
@@ -41,7 +66,8 @@ def ezlog(inputdir):
                     os.makedirs(f"/home/kali/ezlog/{outputdir}/{inputdir}_exported_objects/{inputdir}_{item}")
                 if os.path.exists(f"/home/kali/ezlog/{outputdir}/{inputdir}_exported_objects/{inputdir}_{item}"):
                     shutil.move(f"/home/kali/ezlog/{outputdir}/{other_path}/{filename}", f"/home/kali/ezlog/{outputdir}/{inputdir}_exported_objects/{inputdir}_{item}")
-           # else:
+        
+        # else:
                # if not os.path.exists(other_path):
                 #    os.makedirs(other_path)
                 #if os.path.exists(other_path):
